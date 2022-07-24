40866bc15655deffd3100b0572c7648152b8d138
Pogobanane <aenderboy@gmx.de>
2022-07-24 20:33:46 +0200

    fix npm exports

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,25 +1,41 @@
 const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: {
-      type: 'umd',
-      name: 'sp',
+module.exports = [
+  {
+    target: 'web',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'browser.js',
+      library: {
+        type: 'umd',
+        name: 'sp',
+      },
     },
-  },
-  plugins: [
-		new NodePolyfillPlugin({
-			excludeAliases: ['fs']
-		})
-	],
-  resolve: {
-    fallback: {
-      fs: false,
+    plugins: [
+      new NodePolyfillPlugin({
+        excludeAliases: ['fs']
+      })
+    ],
+    resolve: {
+      fallback: {
+        fs: false,
+      },
     },
+    devtool: "inline-source-map",
   },
-  devtool: "inline-source-map",
-};
+  {
+    target: 'node',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      library: {
+        type: 'umd',
+        name: 'sp',
+      },
+    },
+    devtool: "inline-source-map",
+  }
+];
